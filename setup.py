ea80aa0eb5313e3b38ef3f92e19ce2761045a9ea
Anand Sanmukhani <asanmukh@redhat.com>
2019-03-21 14:57:05 -0400

    Change Package name to `prometheus-api-client`

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ def get_install_requires():
         return [req.split(' ', maxsplit=1)[0] for req in res if req]
 
 setuptools.setup(
-    name="prometheus-connect",
+    name="prometheus-api-client",
     version="0.0.1",
     author="Anand Sanmukhani",
     author_email="asanmukh@redhat.com",
     description="A small python api to collect data from prometheus",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/4n4nd/prometheus-connect",
+    url="https://github.com/AICoE/prometheus-api-client-python",
     install_requires=get_install_requires(),
     packages=setuptools.find_packages(),
     classifiers=[
