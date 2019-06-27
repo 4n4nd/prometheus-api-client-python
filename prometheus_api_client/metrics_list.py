0f412c75e1008fe58e5ddef5f485b670c41063e9
Anand Sanmukhani <asanmukh@redhat.com>
2019-06-27 16:17:50 -0400

    Update docstring for MetricsList class for better sphinx documentation

--- prometheus_api_client/metrics_list.py
+++ prometheus_api_client/metrics_list.py
@@ -4,7 +4,23 @@ from .metric import Metric
 
 
 class MetricsList(list):
-    """docstring for MetricsList."""
+    """A Class to initialize a list of Metric objects at once.
+
+    :param metric_data_list: (list|json) This is an individual metric or list of metrics received
+                             from prometheus as a result of a promql query.
+
+    Example Usage:
+      .. code-block:: python
+
+          prom = PrometheusConnect()
+          my_label_config = {'cluster': 'my_cluster_id', 'label_2': 'label_2_value'}
+          metric_data = prom.get_metric_range_data(metric_name='up', label_config=my_label_config)
+
+          metric_object_list = MetricsList(metric_data) # metric_object_list will be initialized as
+                                                        # a list of Metric objects for all the
+                                                        # metrics downloaded using get_metric query
+
+    """
 
     def __init__(self, metric_data_list):
         # if the input is not a list
