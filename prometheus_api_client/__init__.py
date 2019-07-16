5651e75d9b7bc0068006332ca8024727f6e7c3ec
Anand Sanmukhani <asanmukh@redhat.com>
2019-07-16 10:15:34 -0400

    Update version number for v0.0.2b1 release

--- prometheus_api_client/__init__.py
+++ prometheus_api_client/__init__.py
@@ -1,7 +1,7 @@
 """ A collection of tools to collect and manipulate prometheus metrics"""
 
 __title__ = "prometheus-connect"
-__version__ = "0.0.1"
+__version__ = "0.0.2b1"
 
 from .prometheus_connect import *
 from .metric import Metric
