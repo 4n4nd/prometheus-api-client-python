df36cceffe3646bc0e79e1c1841569cf6a6e04fe
A N U S H <54374648+anushkrishnav@users.noreply.github.com>
2020-11-23 14:27:15 +0000

    feat: replaced exit with ValueError

--- prometheus_api_client/prometheus_connect.py
+++ prometheus_api_client/prometheus_connect.py
@@ -190,7 +190,7 @@ class PrometheusConnect:
         end = round(end_time.timestamp())
 
         if (end_time - start_time).total_seconds() < chunk_size.total_seconds():
-            sys.exit("specified chunk_size is too big")
+            raise ValueError("specified chunk_size is too big")
         chunk_seconds = round(chunk_size.total_seconds())
 
         if label_config:
