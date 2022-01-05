aafd99b2e177d7e0cb90ff8c5aa7275395a4a7be
Alexey Sergeev <alexey.sergeev@nordigy.ru>
2022-01-06 01:00:11 +0300

    Improving the all_metrics function via get_label_values.

--- prometheus_api_client/prometheus_connect.py
+++ prometheus_api_client/prometheus_connect.py
@@ -91,27 +91,14 @@ class PrometheusConnect:
             (RequestException) Raises an exception in case of a connection error
             (PrometheusApiClientException) Raises in case of non 200 response status code
         """
-        params = params or {}
-        response = self._session.get(
-            "{0}/api/v1/label/__name__/values".format(self.url),
-            verify=self.ssl_verification,
-            headers=self.headers,
-            params=params,
-        )
-
-        if response.status_code == 200:
-            self._all_metrics = response.json()["data"]
-        else:
-            raise PrometheusApiClientException(
-                "HTTP Status Code {} ({!r})".format(response.status_code, response.content)
-            )
+        self._all_metrics = self.get_label_values(label_name="__name__", params=params)
         return self._all_metrics
 
     def get_label_values(self, label_name: str, params: dict = None):
         """
         Get a list of all values for the label.
 
-        :label_name: (str) The name of the label for which you want to get all the values.
+        :param label_name: (str) The name of the label for which you want to get all the values.
         :param params: (dict) Optional dictionary containing GET parameters to be
             sent along with the API request, such as "time"
         :returns: (list) A list of names for the label from the specified prometheus host
