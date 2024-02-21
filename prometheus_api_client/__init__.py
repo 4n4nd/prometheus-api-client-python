4ce551d0da6da700f93b79a1ade615d1d57309c7
Anand Sanmukhani <asan@bu.edu>
2024-02-21 16:21:40 -0500

    Update __init__.py
    Update package version to 0.5.5

--- prometheus_api_client/__init__.py
+++ prometheus_api_client/__init__.py
@@ -1,7 +1,7 @@
 """A collection of tools to collect and manipulate prometheus metrics."""
 
 __title__ = "prometheus-connect"
-__version__ = "0.5.4"
+__version__ = "0.5.5"
 
 from .prometheus_connect import *  # noqa F403
 from .metric import Metric  # noqa F401
