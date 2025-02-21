88ae193478d215a269d1fa16f48a37391eaa20f5
TMVKasiViswanath <kasiviswanath4962@gmail.com>
2025-02-21 23:30:05 +0530

    Resolved issue #225

--- prometheus_api_client/__init__.py
+++ prometheus_api_client/__init__.py
@@ -1,10 +1,23 @@
+
 """A collection of tools to collect and manipulate prometheus metrics."""
 
 __title__ = "prometheus-connect"
 __version__ = "0.5.7"
 
-from .prometheus_connect import *  # noqa F403
-from .metric import Metric  # noqa F401
-from .metrics_list import MetricsList  # noqa F401
-from .metric_snapshot_df import MetricSnapshotDataFrame  # noqa F401
-from .metric_range_df import MetricRangeDataFrame  # noqa F401
+def __getattr__(name):
+    if name == "PrometheusConnect":
+        from .prometheus_connect import PrometheusConnect
+        return PrometheusConnect
+    elif name == "Metric":
+        from .metric import Metric
+        return Metric
+    elif name == "MetricsList":
+        from .metrics_list import MetricsList
+        return MetricsList
+    elif name == "MetricSnapshotDataFrame":
+        from .metric_snapshot_df import MetricSnapshotDataFrame
+        return MetricSnapshotDataFrame
+    elif name == "MetricRangeDataFrame":
+        from .metric_range_df import MetricRangeDataFrame
+        return MetricRangeDataFrame
+    raise AttributeError(f"module {__name__} has no attribute {name}")
