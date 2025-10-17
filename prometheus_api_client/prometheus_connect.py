0cdc932dad21258f4e1cc0569206d1f49c2c577d
Titouan <titouanfreville@gmail.com>
2025-10-17 11:43:51 +0200

    feat: Add support for `/series` endpoint

--- prometheus_api_client/prometheus_connect.py
+++ prometheus_api_client/prometheus_connect.py
@@ -117,6 +117,42 @@ class PrometheusConnect:
         self._all_metrics = self.get_label_values(label_name="__name__", params=params)
         return self._all_metrics
 
+
+    def get_series(self, start: datetime, end: datetime, params: dict = None):
+        """
+        Get a list series happening between start and end times.
+
+        :param start: (int) Start time unix ts
+        :param end: (int) End time unix ts
+        :param params: (dict) Optional dictionary containing GET parameters to be
+            sent along with the API request, such as "start", "end" or "match[]".
+        :returns: (list) A list of labels from the specified prometheus host
+        :raises:
+            (RequestException) Raises an exception in case of a connection error
+            (PrometheusApiClientException) Raises in case of non 200 response status code
+        """
+        params = params or {}
+        params["start"] = start.timestamp()
+        params["end"] = end.timestamp()
+        response = self._session.get(
+            "{0}/api/v1/series".format(self.url),
+            verify=self._session.verify,
+            headers=self.headers,
+            params=params,
+            auth=self.auth,
+            cert=self._session.cert,
+            timeout=self._timeout,
+        )
+
+        if response.status_code == 200:
+            labels = response.json()["data"]
+        else:
+            raise PrometheusApiClientException(
+                "HTTP Status Code {} ({!r})".format(response.status_code, response.content)
+            )
+        return labels
+
+
     def get_label_names(self, params: dict = None):
         """
         Get a list of all labels.
